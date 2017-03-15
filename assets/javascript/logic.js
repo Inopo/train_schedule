f1a7e890564e3881adc231e28656c21c6d077116
Ege <ertem@ephesusventures.com>
2017-03-15 06:55:52 -0700

    1)Iwrites trains to firebase2)Todo display the trains in the right html elements 2) Do math

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -18,29 +18,91 @@ Users from many different machines must be able to view same train times.
 Styling and theme are completely up to you. Get Creative!
 
 
-Objective:
-Audience:
+Objective:This is an app that you basically have inputs about trains (objects), and then you simply pool them back.... 
+Audience:Zach 
 
+State 1
 
+You will be able to enter an object with 4 elements to the database. 
 
+State 2
 
+The elements that are in the data base will be presented. 
 
 
 
 */
+// get the document ready for action. This is going to cover the entire "logic"
+
 
     $( document ).ready(function() {
 
+//Create a variable to reference the database
+
     	var database = firebase.database();
 
-    	console.log(database);
-
-
-
+    	 console.log(database);
 
 
 
     console.log( "ready!" );
+
+// ----------------------------------------------------------------------------
+
+// State 1: Here I want to capture the user in put, I can start with humble beginings
+
+//---------------------------------------------------------------------------------
+
+
+//Listen to the submit button click
+//capture the user train name input
+
+
+
+
+//Once the button is clicked, you need to push that input to the database.   
+$("#submit-train").on("click", function() {
+
+    event.preventDefault();
+
+var trainName = $("#train-name").val().trim();
+var destination = $("#destination").val().trim();
+var frequency = $("#frequency").val().trim();
+var firstTrainTime = $("#first-train-time").val().trim();
+
+console.log(trainName);
+console.log(destination);
+console.log(frequency);
+console.log(firstTrainTime);
+
+
+
+    database.ref("/trains").push({
+      
+      trainName: trainName,
+      destination: destination,
+      frequency: frequency,
+      firstTrainTime: firstTrainTime,
+      
+      dateAdded: firebase.database.ServerValue.TIMESTAMP
+
+      
+    });
+
+
+
+
+});
+
+//Firabase watcher + initial loader
+database.ref("/trains").on("child_added", function(snapshot) {
+
+$("#full_train_list").append("<p>"+snapshot.val().trainName+"</p>");
+
+
+});
+
+
 
 
 });
