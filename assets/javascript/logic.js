d497f47d4888c7518d6c15be4edd1ebc62730b99
Ege <ertem@ephesusventures.com>
2017-03-18 14:23:18 -0700

    Last one

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -41,11 +41,8 @@ The elements that are in the data base will be presented.
 
     	var database = firebase.database();
 
-    	 console.log(database);
 
-
-
-    console.log( "ready!" );
+     console.log( "ready!" );
 
 // ----------------------------------------------------------------------------
 
@@ -63,17 +60,23 @@ The elements that are in the data base will be presented.
 //Once the button is clicked, you need to push that input to the database.   
 $("#submit-train").on("click", function() {
 
+
+// Don't refresh the damn page!!!!
     event.preventDefault();
+
+  // This is where you capture the inputs  
 
 var trainName = $("#train-name").val().trim();
 var destination = $("#destination").val().trim();
 var frequency = $("#frequency").val().trim();
 var firstTrainTime = $("#first-train-time").val().trim();
 
-console.log(trainName);
-console.log(destination);
-console.log(frequency);
-console.log(firstTrainTime);
+// Log the 
+
+//console.log(trainName);
+//console.log(destination);
+//console.log(frequency);
+//console.log(firstTrainTime);
 
 
 
@@ -91,18 +94,154 @@ console.log(firstTrainTime);
 
 
 
-
 });
+
+
+
 
 //Firabase watcher + initial loader
-database.ref("/trains").on("child_added", function(snapshot) {
+database.ref("/trains").on("value", function(snapshot) {
 
-$("#full_train_list").append("<p>"+snapshot.val().trainName+"</p>");
 
+	// var test1 = snapshot.child("trainName").val();
+
+	// console.log(test1);
+
+	/*
+
+var myTrainName = database.ref("/trains").child("trainName");
+
+console.log(myTrainName);
+
+var myDestination = snaphot.child("destination").val();
+
+console.log(myDestination); */
+
+
+
+//$("#full_train_list").append("<p>"+snapshot.val().trainName+"</p>");
+
+//create a new line at the table
+var nextArrival = 0;
+var minutesAway = 0;
+var myTrainName = snapshot.val();
+var myDestination = snapshot.child("destination").val();
+var myFrequency = snapshot.child("frequency").val();
+
+console.log("here is myTrainName", myTrainName);
+
+
+var trains = snapshot.val();
+
+var test2 = Object.keys(trains);
+
+console.log ("keys of the obejct",  test2);
+
+
+
+
+console.log("here is trains>>>", trains);
+for (var key in trains){
+
+$("#full-list").append(+"<tr>"+"<td>"+myTrainName+"</td>"+"<td>"+myDestination+"</td>"+"<td>"+myFrequency+"</td>"+"<td>"+nextArrival+"</td>"+"<td>"+minutesAway+"</td>"+"</tr>");
+
+  console.log("key is>>>", key, " and value is>>>>", trains[key])
+}
+
+ [].forEach(function(train) {
+
+   // console.log(trains);
+
+
+
+
+// });
+
+
+
+// $("#full-list").append(+"<tr>"+"<td>"+test1+"</td>"+"<td>"+snapshot.val().destination+"</td>"+"<td>"+snapshot.val().frequency+"</td>"+"<td>"+nextArrival+"</td>"+"<td>"+minutesAway+"</td>"+"</tr>");
+//$("#full-list").append(+"<tr>"+"<td>"+snapshot.val().destination+"</td>"+"</tr>");
+//$("#full-list").append(+"<tr>"+"<td>"+snapshot.val().frequency+"</td>"+"</tr>");
+//$("#full-list").append(+"<tr>"+"<td>"+snapshot.val().firstTrainTime+"</td>"+"</tr>");
 
 });
 
 
 
+// push name etc there
+
 
 });
+
+});
+
+/*
+db = fb.db();
+
+db.ref().on("value", function(snap)) {
+  
+  var trains  = snap.val();
+
+  trains.forEach(function(train) 
+
+  record.starTime; 
+
+
+})
+*/
+
+
+
+
+
+    // **** paste the firebase example here 
+
+
+
+    // ***** Paste the other example here
+/*
+
+    // for each user that is online, output to the console
+	   gameRef.on('value', function(function(gamesSnapshot) {
+	       gamesSnapshot.forEach(function (snapshot) {
+	           var obj = snapshot.val();
+	           if(obj.isOnline == true) {
+	               console.log(obj.name + " is online.");
+	           }
+	       }
+	   }); 
+*/
+
+/*
+
+
+
+    // Assume we have the following data in the Database:
+{
+  "users": {
+    "ada": {
+      "first": "Ada",
+      "last": "Lovelace"
+    },
+    "alan": {
+      "first": "Alan",
+      "last": "Turing"
+    }
+  }
+}
+
+// Loop through users in order with the forEach() method. The callback
+// provided to forEach() will be called synchronously with a DataSnapshot
+// for each child:
+var query = firebase.database().ref("users").orderByKey();
+query.once("value")
+  .then(function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+      // key will be "ada" the first time and "alan" the second time
+      var key = childSnapshot.key;
+      // childData will be the actual contents of the child
+      var childData = childSnapshot.val();
+  });
+});
+
+*/
